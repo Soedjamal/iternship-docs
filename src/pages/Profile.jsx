b93cb31279ef80647c3126ce06a9e0841edbb995
AkmalIhksan <akmalihksan181@gmail.com>
2025-03-08 10:12:35 +0700

    fix: profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,16 +9,30 @@ const Profile = () => {
         <div className="hero-content flex-col lg:flex-row-reverse w-[400px]">
           <div>
             <h1 class="text-5xl font-bold bg-gradient-to-br from-pink-400 to-purple-600 bg-clip-text text-transparent">
-              Shafira Keshya Az Zahra
+              Profile
             </h1>
+            <div className="bio mt-4">
+              <h4 className="text-lg text-zinc-600 font-bold flex">
+                Nama :{" "}
+                <p className="font-normal ml-2">Shafira Keshya Az Zahra</p>
+              </h4>
+              <h4 className="text-lg text-zinc-600 font-bold flex">
+                Sekolah :{" "}
+                <p className="font-normal ml-2">SMK Negeri 5 Kendal</p>
+              </h4>{" "}
+              <h4 className="text-lg text-zinc-600 font-bold flex">
+                Jurusan :{" "}
+                <p className="font-normal ml-2">Rekayasa Perangkat Lunak</p>
+              </h4>{" "}
+            </div>
             <p className="py-6 text-zinc-600">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button className="btn btn-wide btn-secondary">
+            <a href="#laporan-mugas" className="btn btn-wide btn-secondary">
               Lihat Dokumentasi
-            </button>
+            </a>
           </div>
         </div>
       </div>
